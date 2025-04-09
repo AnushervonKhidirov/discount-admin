d3416d8f016ef0b294f691dca2b43b231bf42d28
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-09 13:30:16 +0500

    feat: new prisma error added

--- src/common/helper/exception.helper.ts
+++ src/common/helper/exception.helper.ts
@@ -24,7 +24,19 @@ function prismaException(err: PrismaClientKnownRequestError) {
   const codes: { [key: string]: HttpException } = {
     P2002: new ConflictException(),
     P2025: new NotFoundException(),
+    P2003: new NotFoundException(getFieldName(err.meta?.field_name, 'not found')),
   };
 
+  console.log(
+    'error',
+    typeof err.meta?.field_name === 'string' ? err.meta.field_name.replace('_id', '') : undefined,
+  );
+
   if (codes[err.code]) return codes[err.code];
+}
+
+function getFieldName(field?: unknown, additionalText?: string) {
+  if (typeof field !== 'string' || field.length === 0) return;
+  const firstLatter = field[0]?.toUpperCase();
+  return `${firstLatter}${field.slice(1)} ${additionalText}`.replace('_id', '');
 }
