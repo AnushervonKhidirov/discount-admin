b41f5da0620035223936a7ec9ae1a9518ae70999
Anushervon Khidirov <ankhid98@gmail.com>
2025-04-18 23:33:43 +0500

    fix: update bank type updated

--- src/bank/dto/update-bank.dto.ts
+++ src/bank/dto/update-bank.dto.ts
@@ -2,7 +2,7 @@ import { t } from 'elysia';
 
 export const updateBankBody = t.Object({
   name: t.Optional(t.String()),
-  logoUrl: t.Optional(t.String()),
+  archived: t.Optional(t.Boolean()),
 });
 
 export type UpdateBankDto = typeof updateBankBody.static;
